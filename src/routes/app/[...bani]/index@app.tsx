f425baab5e1d27a82ab10251e5b6e9e7d76d1898
Bhajneet S.K <bhajneet@gmail.com>
2023-10-28 10:59:25 -0400

    feat: add 404 support to app

--- src/routes/app/[...bani]/index@app.tsx
+++ src/routes/app/[...bani]/index@app.tsx
@@ -6,42 +6,58 @@ import Line from '~/components/line/line'
 export const useApi = routeLoader$(async (requestEvent) => {
   const bani = requestEvent.params.bani
   const response = await fetch(`https://www.shabados.com/api/app/${bani}`)
-  const data = await response.json()
-  return data
+  if (response.status !== 404) {
+    const data = await response.json()
+    return data
+  }
+  return null
 })
 
 export const head: DocumentHead = ({ resolveValue }) => {
   const res = resolveValue(useApi)
+  if (res !== null) {
+    return {
+      title: `${res.title.unicode} - Shabad OS`,
+      meta: [
+        {
+          name: 'description',
+          content: `Nitnem Bāṇī ${res.title.roman}`,
+        },
+      ],
+    }
+  }
   return {
-    title: `${res.title.unicode} - Shabad OS`,
-    meta: [
-      {
-        name: 'description',
-        content: `Nitnem Bāṇī ${res.title.roman}`,
-      },
-    ],
+    title: `404 - Shabad OS`,
   }
 }
 
 export default component$(() => {
   const data = useApi()
+  if (data.value !== null) {
+    return (
+      <>
+        {data.value.data.map(({ id, src, pronunciation, translation }: any) => (
+          <Line
+            key={id}
+            id={id}
+            src={src}
+            pronunciation={pronunciation}
+            translation={translation}
+          />
+        ))}
+        <BottomBar
+          prevLink={
+            data.value.paging?.previous && `/app/${data.value.paging.previous}`
+          }
+          nextLink={data.value.paging?.next && `/app/${data.value.paging.next}`}
+        />
+      </>
+    )
+  }
   return (
     <>
-      {data.value.data.map(({ id, src, pronunciation, translation }: any) => (
-        <Line
-          key={id}
-          id={id}
-          src={src}
-          pronunciation={pronunciation}
-          translation={translation}
-        />
-      ))}
-      <BottomBar
-        prevLink={
-          data.value.paging?.previous && `/app/${data.value.paging.previous}`
-        }
-        nextLink={data.value.paging?.next && `/app/${data.value.paging.next}`}
-      />
+      <h1>404</h1>
+      <p>Not found</p>
     </>
   )
 })
