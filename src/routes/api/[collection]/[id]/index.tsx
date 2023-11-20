26722eb5a81c4d73142f83e6288c50d5837579bb
Bhajneet S.K <bhajneet@gmail.com>
2023-11-20 11:29:51 -0500

    feat: add firestore api

---
+++ src/routes/api/[collection]/[id]/index.tsx
@@ -0,0 +1,32 @@
+import { type RequestHandler } from '@builder.io/qwik-city';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../../../lib/firestore';
+
+const collectionMap = new Map<string, string>([
+  ['search', 'search'],
+  ['l', 'lines'],
+  ['g', 'line-groups'],
+]);
+
+export const onGet: RequestHandler = async ({ params, json, cacheControl }) => {
+  const id = params.id;
+  const collection = collectionMap.get(params.collection) as string;
+  const docRef = doc(db, collection, id);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    json(200, await docSnap.data());
+  } else {
+    json(204, { message: '204 No Content' });
+  }
+  cacheControl({
+    staleWhileRevalidate: 60 * 60 * 24 * 7 * 2,
+    maxAge: 60 * 60 * 24 * 2,
+  });
+  cacheControl(
+    {
+      staleWhileRevalidate: 60 * 60 * 24 * 7 * 2,
+      maxAge: 60 * 60 * 24 * 2,
+    },
+    'CDN-Cache-Control',
+  );
+};
