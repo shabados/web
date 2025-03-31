f92ed143303abd0e4887abf20c01d356da5bce82
Bhajneet S.K <bhajneet@gmail.com>
2025-03-31 18:27:34 -0400

    feat: improve hukamnama and title display

--- src/components/hukamnama/hukamnama.tsx
+++ src/components/hukamnama/hukamnama.tsx
@@ -1,76 +1,13 @@
 import { component$, useStylesScoped$ } from '@builder.io/qwik';
 import styles from './hukamnama.css?inline';
+import isTitle, { isEndOfPauri } from '~/lib/isTitle';
 
 export default component$(({ data }: any) => {
   useStylesScoped$(styles);
-  const titlesFuzzy = [
-    'ੴ',
-    '॥ ਜਪੁ ॥',
-    'ਪਾਤਿਸਾਹੀ ੧੦',
-    '॥ ਤ੍ਵ ਪ੍ਰਸਾਦਿ ॥',
-    '॥ ਤ੍ਵ ਪ੍ਰਸਾਦਿ ਕਥਤੇ ॥',
-    'ਭੁਜੰਗ ਪ੍ਰਯਾਤ ਛੰਦ ॥',
-    '॥ ਚਾਚਰੀ ਛੰਦ ॥',
-    '॥ ਸਵੱਯੇ ॥',
-    '॥ ਚੌਪਈ ॥',
-    'ਮਹਲਾ ੧',
-    'ਮਹਲਾ ੨',
-    'ਮਹਲਾ ੩',
-    'ਮਹਲਾ ੪',
-    'ਮਹਲਾ ੫',
-    'ਮਹਲਾ ੬',
-    'ਮਹਲਾ ੭',
-    'ਮਹਲਾ ੮',
-    'ਮਹਲਾ ੯',
-    'ਮਃ ੧',
-    'ਮਃ ੨',
-    'ਮਃ ੩',
-    'ਮਃ ੪',
-    'ਮਃ ੫',
-    'ਮਃ ੬',
-    'ਮਃ ੭',
-    'ਮਃ ੮',
-    'ਮਃ ੯',
-    'ਦੇਵਗੰਧਾਰੀ ੧',
-    'ਦੇਵਗੰਧਾਰੀ ੨',
-    'ਦੇਵਗੰਧਾਰੀ ੩',
-    'ਦੇਵਗੰਧਾਰੀ ੪',
-    'ਦੇਵਗੰਧਾਰੀ ੫',
-    'ਦੇਵਗੰਧਾਰੀ ੬',
-    'ਦੇਵਗੰਧਾਰੀ ੭',
-    'ਦੇਵਗੰਧਾਰੀ ੮',
-    'ਦੇਵਗੰਧਾਰੀ ੯',
-  ];
-  const titlesExact = [
-    'ਜਾਪੁ ॥',
-    'ਸਲੋਕੁ ॥',
-    'ਚਾਚਰੀ ਛੰਦ ॥',
-    'ਏਕ ਅਛਰੀ ਛੰਦ ॥',
-    'ਸ੍ਵੈਯਾ ॥',
-    'ਦੋਹਰਾ ॥',
-    'ਸ੍ਰੀ ਭਗਉਤੀ ਜੀ ਸਹਾਇ ॥',
-    'ਵਾਰ ਸ੍ਰੀ ਭਗਉਤੀ ਜੀ ਕੀ ॥',
-    'ਰਹਰਾਸਿ ਸਾਹਿਬ',
-    'ਪਉੜੀ।',
-    'ਪਉੜੀ ।',
-    'ਪਉੜੀ॥',
-    'ਪਉੜੀ ॥',
-    'ਆਸਾ ॥',
-    'ਅਸਟਪਦੀ ॥',
-    // add screensaver / slideshow values here
-    'ਵਾਹਿਗੁਰੂ',
-    'ਵਾਹਿਗੁਰੂ ਜੀ ਕਾ ਖ਼ਾਲਸਾ; ਵਾਹਿਗੁਰੂ ਜੀ ਕੀ ਫ਼ਤਿਹ',
-    'ਬੋਲੇ ਸੋ ਨਿਹਾਲ; ਸਤਿ ਸ੍ਰੀ ਅਕਾਲ',
-  ];
-  const isTitle = (string: string): boolean =>
-    [...titlesFuzzy, ...titlesExact].some((ele) => string.includes(ele));
-
-  const isEndOfPauri = (string: string): boolean =>
-    /॥[੧੨੩੪੫੬੭੮੯੦]+॥/.test(string) ||
-    ['ਬੋਲੋ ਜੀ ਵਾਹਿਗੁਰੂ ।', '॥ ਰਹਾਉ ॥'].some((ele) => string.includes(ele));
 
   const remapped = data.data.default.src.map((obj: any) => {
     const src = obj.src.data;
+
     const title = isTitle(src);
     const endOfPauri = isEndOfPauri(src);
 
@@ -120,38 +57,42 @@ export default component$(({ data }: any) => {
         const lines = Array.isArray(line[0].src) ? line[0].src : [line[0].src];
         return (
           <div key={index} class={`line ${line[0].title ? 'first' : ''}`}>
-            <p>
-              {line[0].Guru} <span class='notes'>{line[0].notes}</span>
-            </p>
             <div>
-              {lines.map((ln: any) => (
-                <p class={`bold ${line[0].title ? 'title' : ''}`} key={1}>
-                  {ln.split(' ').map((word: string, index: number) => {
-                    const betweenWords = index === 0 ? '' : ' ';
-                    const className = word.endsWith(';')
-                      ? 'heavy'
-                      : word.endsWith(',')
-                      ? 'medium'
-                      : word.endsWith('.')
-                      ? 'light'
-                      : '';
-                    const wordContent = [';', ',', '.'].some((char) =>
-                      word.endsWith(char),
-                    )
-                      ? word.slice(0, -1)
-                      : word;
+              <p>
+                {lines.map((ln: any) => (
+                  <span class={`bold ${line[0].title ? 'title' : ''}`} key={1}>
+                    {ln.split(' ').map((word: string, index: number) => {
+                      const betweenWords = index === 0 ? '' : ' ';
+                      const className = word.endsWith(';')
+                        ? 'heavy'
+                        : word.endsWith(',')
+                        ? 'medium'
+                        : word.endsWith('.')
+                        ? 'light'
+                        : '';
+                      const wordContent = [';', ',', '.'].some((char) =>
+                        word.endsWith(char),
+                      )
+                        ? word.slice(0, -1)
+                        : word;
 
-                    return (
-                      <>
-                        {betweenWords}
-                        <span class={className}>{wordContent}</span>
-                      </>
-                    );
-                  })}
-                </p>
-              ))}
+                      return (
+                        <>
+                          {betweenWords}
+                          <span class={className}>{wordContent}</span>
+                        </>
+                      );
+                    })}
+                  </span>
+                ))}
+              </p>
             </div>
-            <p>{line[0].en}</p>
+            <div>
+              <p>
+                {line[0].Guru} <span class='notes'>{line[0].notes}</span>
+              </p>
+              <p>{line[0].en}</p>
+            </div>
           </div>
         );
       })}
